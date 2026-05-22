5f55e5024a0b9c2d03f65d3afd52b89e51a317fb
Rahul Bhargav <rahulbhargav24@gmail.com>
2026-05-22 23:54:16 +0530

    update

--- IMS Frontend/src/main.jsx
+++ IMS Frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import AuthProvider from './context/AuthContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import AuthProvider from './context/AuthContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <AuthProvider> 
       <App />
     </AuthProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
