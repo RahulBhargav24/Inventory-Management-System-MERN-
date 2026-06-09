c636148d5a7ea8bf288382e34f6a70b1d2ac0921
Rahul Bhargav <rahulbhargav24@gmail.com>
2026-06-09 05:36:27 +0530

    update

--- IMS Frontend/src/App.jsx
+++ IMS Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router'
-import Root from './components/Root'
+import Root from './utils/Root'
 import Login from './pages/Login'
+import ProtectedRoutes from './utils/ProtectedRoutes'
 
 function App() {
 
@@ -9,9 +10,12 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
-        <Route path="/admin/dashboard" element={<h1>admin dashboard</h1>} />
+        <Route path="/admin/dashboard" element={<ProtectedRoutes requireRole={["admin"]}>
+          <h1>admin dashboard</h1>
+        </ProtectedRoutes>} />
         <Route path="/customer/dashboard" element={<h1>customer dashboard</h1>} />
         <Route path="/login" element={<Login />} />
+        <Route path="/unauthorized" element={<p className='font-bold text-3xl mt-20 ml-20'>Unauthorized</p>} />
       </Routes>
     </Router>
   )
